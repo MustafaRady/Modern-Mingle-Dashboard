ecbbae4cb5a4b365cbba8c5be061921b20a6cc8b
MustafaRady <116670293+MustafaRady@users.noreply.github.com>
2024-06-12 22:20:37 +0300

    removing login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,34 @@ import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './pages/users/users.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
-import { AuthGuard } from './auth.guard';
+// import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './pages/auth/auth.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 
+// const routes: Routes = [
+//   { path: '', redirectTo: '/products', pathMatch: 'full' }, // Default route
+//   {path:'users',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true } },
+//   {path:'products',component:ProductsComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
+//   {path:'dashboard',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
+//   {path:'inventory',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
+//   {path:'orders',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
+//   {path:'analytics',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
+//   {path:'promotions',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
+//   {path:'login',component:AuthComponent,canActivate:[AuthGuard],data: { requiresLogin: false }},
+//   {path:'**',component:NotFoundPageComponent}
+// ];
+
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' }, // Default route
-  {path:'users',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true } },
-  {path:'products',component:ProductsComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
-  {path:'dashboard',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
-  {path:'inventory',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
-  {path:'orders',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
-  {path:'analytics',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
-  {path:'promotions',component:ComingSoonComponent,canActivate:[AuthGuard],data: { requiresLogin: true }},
-  {path:'login',component:AuthComponent,canActivate:[AuthGuard],data: { requiresLogin: false }},
+  {path:'users',component:ComingSoonComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'dashboard',component:ComingSoonComponent},
+  {path:'inventory',component:ComingSoonComponent},
+  {path:'orders',component:ComingSoonComponent},
+  {path:'analytics',component:ComingSoonComponent},
+  {path:'promotions',component:ComingSoonComponent},
+  {path:'login',component:AuthComponent},
   {path:'**',component:NotFoundPageComponent}
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,11 @@ export class AppComponent implements OnInit
   }
 
   ngOnInit(): void {
-    this.authService.getUser().subscribe(
-      user => {
-        console.log(user)
-        this.isAuthenticated = !!user
-      }
-    )
+    // this.authService.getUser().subscribe(
+    //   user => {
+    //     console.log(user)
+    //     this.isAuthenticated = !!user
+    //   }
+    // )
   }
 }
